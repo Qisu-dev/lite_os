d6601eb9367f4f613b546edb6d51d4030d49228d
Jason Youngberg <jasondyoungberg@gmail.com>
2025-03-07 00:11:51 +0000

    Update for limine 0.4

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -3,30 +3,30 @@
 
 use core::arch::asm;
 
-use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
 use limine::BaseRevision;
+use limine::request::{FramebufferRequest, RequestsEndMarker, RequestsStartMarker};
 
 /// Sets the base revision to the latest revision supported by the crate.
 /// See specification for further info.
 /// Be sure to mark all limine requests with #[used], otherwise they may be removed by the compiler.
 #[used]
 // The .requests section allows limine to find the requests faster and more safely.
-#[link_section = ".requests"]
+#[unsafe(link_section = ".requests")]
 static BASE_REVISION: BaseRevision = BaseRevision::new();
 
 #[used]
-#[link_section = ".requests"]
+#[unsafe(link_section = ".requests")]
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
 /// Define the stand and end markers for Limine requests.
 #[used]
-#[link_section = ".requests_start_marker"]
+#[unsafe(link_section = ".requests_start_marker")]
 static _START_MARKER: RequestsStartMarker = RequestsStartMarker::new();
 #[used]
-#[link_section = ".requests_end_marker"]
+#[unsafe(link_section = ".requests_end_marker")]
 static _END_MARKER: RequestsEndMarker = RequestsEndMarker::new();
 
-#[no_mangle]
+#[unsafe(no_mangle)]
 unsafe extern "C" fn kmain() -> ! {
     // All limine requests must also be referenced in a called function, otherwise they may be
     // removed by the linker.
@@ -40,7 +40,13 @@ unsafe extern "C" fn kmain() -> ! {
                 let pixel_offset = i * framebuffer.pitch() + i * 4;
 
                 // Write 0xFFFFFFFF to the provided pixel offset to fill it white.
-                *(framebuffer.addr().add(pixel_offset as usize) as *mut u32) = 0xFFFFFFFF;
+                unsafe {
+                    framebuffer
+                        .addr()
+                        .add(pixel_offset as usize)
+                        .cast::<u32>()
+                        .write(0xFFFFFFFF)
+                };
             }
         }
     }
